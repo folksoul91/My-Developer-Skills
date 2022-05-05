83498d3b8a924dd0fe45f1d2f689e6904c5969b4
Dane Kim <mr.dane@danes-mbp.lan>
2022-05-05 18:56:42 -0400

    created a remove fn in jquery click event

--- app.js
+++ app.js
@@ -2,4 +2,11 @@ $("#btn").click(() => {
   let userInput = $("#user-input").val();
   let addTableRows = `<tr><td><button class='delete'>X</button></td><td>${userInput}</td></tr>`;
   $("#table").append(addTableRows);
+  $("#user-input").val("");
+});
+
+// Create a fn to remove the table rows
+$("#table").on("click", "button", function () {
+  let rows = $(this).closest("tr");
+  rows.remove();
 });
